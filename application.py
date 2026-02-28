bef6dd221143178ce74efdf59ec1ea965f582003
Dhairya-45 <875dharyaag@gmail.com>
2026-02-28 21:12:49 +0530

    Update application file

--- application.py
+++ application.py
@@ -1,4 +1,5 @@
 import pickle
+import os
 import numpy as np
 from flask import Flask, request, render_template
 
@@ -63,5 +64,5 @@ def predict_datapoint():
 
 
 if __name__ == "__main__":
-    app.run()
-
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
